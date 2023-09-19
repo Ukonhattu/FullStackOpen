7e39a885687a1860b8cf456f15679a92367519dd
Daniel Koch <danielkoch@Daniels-MacBook-Air.local>
2023-09-19 15:34:52 +0300

    kurssitiedot 1.1 1.2

---
+++ Tehtävät/par1/kurssitiedot/src/App.jsx
@@ -0,0 +1,46 @@
+
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => { 
+  return (
+    props.parts.map((part) => {
+      return (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      )
+    })
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [{name: "Fundamentals of React", exercises: 10}, 
+                    {name: "Using props to pass data", exercises: 7}, 
+                    {name: "State of a component", exercises: 14}]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts}/>
+    </div>
+  )
+}
+
+export default App
